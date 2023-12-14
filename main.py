b852b4481e963b86f7e2e40c106a2868a5f1f939
Farbod Parkhooi <farbod.p1390@gmail.com>
2023-12-14 15:20:04 +0330

    add thumbnail

---
+++ main.py
@@ -0,0 +1,4 @@
+from faker import Faker
+from tkinter import Tk
+root = Tk()
+faker = Faker()
---
+++ script.py
@@ -0,0 +1,6 @@
+class Create_profile():
+    def __init__(self, gender: str, Name: any, Job: any, email: any, website: any):
+        self.gender = gender
+        self.name = Name
+        self.job = Job
+        self.email = email
